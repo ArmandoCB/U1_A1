e22292f27c7806f954a054b85f2f34e49e41747e
Armando Casimiro <armandocb@comunidad.unam.mx>
2018-08-26 13:39:02 -0500

    Initial commit

---
+++ app/src/main/java/com/example/armyn/unidad1_actividad1/Unidad1_Actividad1.java
@@ -0,0 +1,13 @@
+package com.example.armyn.unidad1_actividad1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Unidad1_Actividad1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_unidad1__actividad1);
+    }
+}
